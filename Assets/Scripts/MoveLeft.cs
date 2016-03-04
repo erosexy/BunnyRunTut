c69e1cc479147b1d29fb873b8a58924e11fbd741
erosexy <carlos_potencia@hotmail.com>
2016-03-04 14:32:17 -0300

    Added Death Animation, Score, Enemies and Enemy Collision

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeft : MonoBehaviour {
+
+	public float speed= 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transforma a posiçao do cacto para que ele se mova para esquerda
+		transform.position += Vector3.left * speed *(Time.deltaTime);
+	}
+}
