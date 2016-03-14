81579a6d8929cd9e71960d3ba9a5a38f6faad7e3
Shianderu <carlos_potencia@hotmail.com>
2016-03-14 12:27:48 -0300

    Added Title Screen and a new Scene for other charactert

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -28,6 +28,11 @@ public class BunnyController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.LoadLevel("Title");
+        }
+
 		if (bunnyHurtTime == -1) {
 
 			//se a tecla "espaço" for pressionada, o sprite pula
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //se Esc ou botão "back" do Android for pressionado
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //sai do jogo
+            Application.Quit();
+        }
+	
+	}
+
+    public void StartGame()
+    {
+        Application.LoadLevel("GameBunny");
+    }
+    
+}
